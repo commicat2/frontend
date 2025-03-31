c49cb39620bbadcba4b3c068df39d5d27158c68d
BakJiSeok <commicat2@gmail.com>
2025-03-31 20:45:17 +0900

    chore: after merge

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,6 +1,16 @@
 'use client'
 
+import {
+  Suspense, lazy, useEffect, useState,
+} from 'react'
 import { QueryClient, QueryClientProvider, isServer } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+declare global {
+  interface Window {
+    toggleDevtools: () => void
+  }
+}
 
 const makeQueryClient = () => {
   return new QueryClient({
@@ -20,9 +30,33 @@ const getQueryClient = () => {
   return browserQueryClient
 }
 
+const ReactQueryDevtoolsProduction = lazy(async () => {
+  return import('@tanstack/react-query-devtools/production').then((d) => {
+    return {
+      default: d.ReactQueryDevtools,
+    }
+  })
+})
+
 const Providers = ({ children }: Children) => {
   const queryClient = getQueryClient()
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  const [showDevtools, setShowDevtools] = useState(false)
+
+  useEffect(() => {
+    window.toggleDevtools = () => { setShowDevtools((old) => { return !old }) }
+  }, [])
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
+    </QueryClientProvider>
+  )
 }
 
 export default Providers
